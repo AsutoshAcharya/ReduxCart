68800c14d2766350946b3c76f5667337acb83c82
Asutosh Acharya <asutosh246@gmail.com>
2022-11-19 23:03:59 +0530

    initial commit

---
+++ src/components/Shop/ProductItem.js
@@ -0,0 +1,21 @@
+import Card from '../UI/Card';
+import classes from './ProductItem.module.css';
+
+const ProductItem = ({ title, price, description }) => {
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
